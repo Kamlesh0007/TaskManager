23055757f0400e2b90c6c1f79300699549f614f7
Kamlesh0007 <wwwkamleshyadav732@gmail.com>
2026-02-22 11:18:08 +0530

    updated apikey

--- Backend/libs/sendEmail.js
+++ Backend/libs/sendEmail.js
@@ -1,31 +1,25 @@
-import sgMail from "@sendgrid/mail";
+import { Resend } from "resend";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-sgMail.setApiKey(process.env.SEND_GRID_API);
+const resend = new Resend(process.env.SEND_GRID_API);
 
 const fromEmail = process.env.FROM_EMAIL;
 
 export const sendEmail = async (to, subject, html) => {
-  const msg = {
-    to,
-    from: `TaskHub <${fromEmail}>`,
-    subject,
-    html,
-  };
-
   try {
-    await sgMail.send(msg);
-    console.log("Email sent successfully");
+    await resend.emails.send({
+      from: `TaskHub <${fromEmail}>`,
+      to,
+      subject,
+      html,
+    });
 
+    console.log("Email sent successfully");
     return true;
   } catch (error) {
     console.error("Error sending email:", error);
-     if (error.response) {
-    console.error("SendGrid response body:", error.response.body);
-  }
-
     return false;
   }
 };
